a984175e2f29721b2ac3c08adeb22e120b3c4df5
Mr. Horace <horacenehemiah@yahoo.com>
2018-01-16 23:07:26 -0600

    Updated status bar color

--- HistoricalBlackFigures/AppDelegate.swift
+++ HistoricalBlackFigures/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.statusBarStyle = .lightContent
         UNUserNotificationCenter.current().delegate = self
         FIRApp.configure()
         FIRDatabase.database().persistenceEnabled = true
@@ -33,27 +34,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-func applicationWillResignActive(_ application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
 
-// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-// Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should usethis method to pause the game.
-}
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should usethis method to pause the game.
+    }
 
-func applicationDidEnterBackground(_ application: UIApplication) {
-// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
 
-func applicationWillEnterForeground(_ application: UIApplication) {
-// Called as part of the transition from the background to the active state; here you can undo many of the changes madeon entering the background.
-UIApplication.shared.applicationIconBadgeNumber = 0
-}
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes madeon entering the background.
+    UIApplication.shared.applicationIconBadgeNumber = 0
+    }
 
-func applicationDidBecomeActive(_ application: UIApplication) {
-}
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
 
-func applicationWillTerminate(_ application: UIApplication) {
-}
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
 
     func generateRandomNumber() {
         let reference = FIRDatabase.database().reference()
